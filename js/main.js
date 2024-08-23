419644115789a63d83a3cb90f51a5141aa456ffa
Jeudy Robles <jeudyjr@outlook.com>
2024-08-22 21:35:07 -0600

    Se agrega funciones JavaScript y se modifican temas menores en el css, html y js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,32 @@
 function functionEncriptar() {
-    alert('Funciona');
+    //alert('Funciona');
+    contador = 0; // Contador siempre el cero
+    contador ++; // Cuando se ejecute la funcion el contador suma uno
 
-    let encriptar = document.getElementById("message-input").value;
-    console.log(encriptar);
+    let textoNoEncriptado = document.getElementById("message-input").value; // Agarra el texto del textarea
+    console.log(textoNoEncriptado); // Lo muestra en la consola
 
-    let desencriptado = document.getElementById("textoEncriptado");
+    textoEncriptado = textoNoEncriptado // Remplaza las vocales con palabras aleatorias***
+        .replace(/e/g, "enter") // /e/: Esto es una expresión regular que busca la letra “e”.
+        .replace(/i/g,  "imes") //  g: Este es un modificador que significa “global”. Indica que la búsqueda debe realizarse en toda la cadena y no detenerse después de la primera coincidencia.
+        .replace(/a/g,    "ai") // "enter": Es la cadena de texto que reemplazará cada ocurrencia de “e, i, a, o, u”.
+        .replace(/o/g,  "ober")
+        .replace(/u/g,  "ufat")
+    document.getElementById("textoEncriptado").textContent = textoEncriptado; // Manda el texto de la variable a la etiqueta p del sidebar
+
+    if (contador ++) { // Condicional si
+        document.getElementById("h2-display").style.display = 'none'; // La propiedad style.display, elimina el elemento de destino del flujo normal de la página y permite que el resto de los elementos ocupen su espacio.
+        document.getElementById("image-muñeco").style.display = 'none'; // Elimina el div de la imagen
+    }
+
+    //console.log(textoEncriptado);
+
+}
+
+// Lo mismo pero al revez
+function functionDesencriptar() {
+    contador = 0;
+    contador ++;
+    
     
 }
